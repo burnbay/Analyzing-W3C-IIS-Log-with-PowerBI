aa2382c200922327876af55f5846ff82b783da68
Erling Brandvik <Erling.Brandvik@sr-bank.no>
2020-10-30 10:18:49 +0100

    Ferdig eksempel med IIS-logger fra AWFAP132

--- Convert w3c log to csv sequentially.bat
+++ Convert w3c log to csv sequentially.bat
@@ -1,4 +1,4 @@
- cd "C:\mstemp\Blog\Sample W3C IIS Logs"
+ cd "E:\Dev\EB\github\burnbay\Analyzing-W3C-IIS-Log-with-PowerBI\Sample W3C IIS Logs"
  for /R %%f in (*.log) do (
  "C:\Program Files (x86)\Log Parser 2.2\"logparser -i:W3C -o:csv "SELECT * INTO '%%f.csv' FROM '%%f'"
  )
